48a208239ab9696e8cfdebbdd06c7b30965848f3
Ozioma Chukwukeme <oziomarc@gmail.com>
2022-11-03 09:11:34 -0400

    Added Procfile

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const homePage = require('./routes/index.js')
+
+app.use('/', homePage)
+
+app.listen(port, () => (
+    console.log(`Example app listening on port ${port}`)
+))
